3e3688853a6bead9abcb5f2e6e38ca08321a4a52
gotticketsto360tour <werling1407@gmail.com>
2020-09-07 10:50:39 +0200

    alright

---
+++ MikkelsStuff/PythonQuiz.py
@@ -0,0 +1,61 @@
+# %%
+
+# TASK 1:
+def add(x, y):
+    return x + y
+
+
+# %%
+
+# TASK 2:
+def count_string(string, letter):
+    return string.lower().count(letter.lower())
+
+
+# %%
+# TASK 3:
+
+# a)
+
+def isEven(i):
+    return i % 2 == 0
+
+
+# %%
+# b)
+
+def isEvenList(listing):
+    return len([x for x in listing if isEven(x)])
+
+
+# %%
+# TASK 4:
+
+def find_max(listing):
+    current_max = listing[0]
+    for element in listing:
+        if element > current_max:
+            current_max = element
+    return current_max
+
+
+# %%
+
+# TASK 5:
+
+def print_word(word):
+    while word:
+        print(word)
+        word = word[:-1]
+
+
+def print_word_rec(word):
+    if word:
+        print(word)
+        word = word[:-1]
+        print_word_rec(word)
+
+
+# TASK 6:
+def get_keys(dic):
+    return list(dic.keys())
